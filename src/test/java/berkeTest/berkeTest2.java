e9ad7d0ecbe9ac27f4cc1309ad56b6e272401656
Berke <93223660+quatronostro@users.noreply.github.com>
2023-05-26 14:15:31 +0300

    berke 26/05 v.1

---
+++ src/test/java/berkeTest/berkeTest2.java
@@ -0,0 +1,13 @@
+package berkeTest;
+
+public class berkeTest2 {
+    public static String str;
+    public berkeTest2(){
+
+    }
+
+    public static void toStr(){
+        str = "string";
+        System.out.println(str);
+    }
+}
--- src/test/java/berkeTest/berketest1.java
+++ src/test/java/berkeTest/berketest1.java
@@ -4,5 +4,7 @@ public class berketest1 {
     public static void main(String[] args) {
         System.out.println("burası berke test 1");
         String name = "berke";
+
+        berkeTest2.toStr();
     }
 }
