ff596b141643c2243bfaae29c3c25c53f8e16ac5
Berke <93223660+quatronostro@users.noreply.github.com>
2023-05-29 15:56:01 +0300

    berke 29/05 v.1

--- src/test/java/berkeTest/berketest1.java
+++ src/test/java/berkeTest/berketest1.java
@@ -1,5 +1,7 @@
 package berkeTest;
 
+import akifTest.Test02;
+
 public class berketest1 {
     public static void main(String[] args) {
         System.out.println("burası berke test 1");
@@ -10,5 +12,9 @@ public class berketest1 {
         System.out.println(name);
 
         System.out.println("hello world");
+
+
+        Test02 test02 = new Test02();
+
     }
 }
